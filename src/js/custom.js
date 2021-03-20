3f2d1e0851e3c9da8c68dc711d4a0286c0075b13
digitalbrush <titus.nderitu@gmail.com>
2021-03-20 17:25:23 +0000

    Theme JS file edits moved to customJS

--- src/js/custom.js
+++ src/js/custom.js
@@ -92,6 +92,22 @@ jQuery(function($){
         
     } 
 
+    var $ = jQuery;
+    if ($('.set_custom_images').length > 0) {
+        if ( typeof wp !== 'undefined' && wp.media && wp.media.editor) {
+            $(document).on('click', '.set_custom_images', function(e) {
+                e.preventDefault();
+                var button = $(this);
+                var id = button.prev();
+                wp.media.editor.send.attachment = function(props, attachment) {
+                    id.val(attachment.url);
+                };
+                wp.media.editor.open(button);
+                return false;
+            });
+        }
+    }
+
 
 });
 
