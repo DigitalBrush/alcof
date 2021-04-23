f048214c594d00728181e2122c15b2369748ee01
Pascal Njue <njuepascal@gmail.com>
2021-04-23 08:28:24 +0300

    add home with full screen scroll

---
+++ global-templates/home-hero.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Hero setup
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+	?>
+
+	<section class="content-section">
+		<div class="container-fluid vh-100" id="wrapper-static-content" tabindex="-1">
+
+			<div class="row hero-header">
+				<div class="col-sm-6 hero-text">
+					<div class="slider-text">
+						<h2 class="big-heading"><?php echo get_option('hero_intro_heading');?></h2>
+						<div class="slider-body">
+							<p class="hero-intro-text"><?php echo get_option('hero_intro_text');?></p>
+							<a href="<?php echo esc_url(home_url('shop')); ?>" class="btn btn-lg btn-secondary">Acheter maintenant</a>
+						</div>
+						<h4 class="small-heading"><?php echo get_option('hero_intro_subheading');?></h4>
+					</div>
+				</div>
+				<div class="col-sm-6 hero-image">
+					<div class="slider-image">
+						<img src="<?php
+						echo  get_option('hero_header_img');
+						?>" style="width:100%" id="hero-header-img"/>
+					</div>
+					<div class="slider-logo">
+						<img class="" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-fichet.svg" height="50"/>
+					</div>
+				</div>
+			</div>
+
+		</div>
+</section>
---
+++ header-small.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar">
+
+		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'alcof' ); ?></a>
+
+
+
+	</div><!-- #wrapper-navbar end -->
