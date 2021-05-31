43ea6c734b8239e494430f130b5bde6a84253d6a
digitalbrush <titus.nderitu@gmail.com>
2021-06-01 00:10:29 +0100

    Updated fullscreen menu nav

--- global-templates/fullscreen-menu.php
+++ global-templates/fullscreen-menu.php
@@ -17,30 +17,83 @@ defined( 'ABSPATH' ) || exit;
 					<button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="material-icons">close</span></button>
 				</div>
 				<div class="modal-body">
-					<div class="container nav-overlay">
-						<div class="row">
-							<div class="col-sm">
-                <h2 class="menu-heading">Tous nos produits</h2>
+					<div class="container-fluid nav-overlay">
+						<div class="row h-100">
+							<div class="col-sm-3">
+                				<span class="menu-heading">Tous nos produits</span>
 							</div>
-							<div class="col-sm">
-                <div class="nav-full">
-                  <div class="nav-full-icon">
-                    One of three columns
-                  </div>
-                  <div class="nav-full-item">
-                    One of three columns
-                  </div>
-                </div>
+							<div class="col-sm nav-column">
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('produits-catégorie/porte-blindee')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-porte-blinde.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Porte blindée
+									</div>
+								</a>
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('produits-catégorie/alarme')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-alarme.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Alarme
+									</div>
+								</a>
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('produits-catégorie/coffre-fort')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-coffre-fort.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Coffre-fort
+									</div>
+								</a>
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('#')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-syndics.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Produits pour syndics
+									</div>
+								</a>
 							</div>
-							<div class="col-sm">
-								One of three columns
+							<div class="col-sm nav-column">
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('produits-catégorie/serrurerie')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-serrure.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Serrure
+									</div>
+								</a>
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('produits-catégorie/videosurveillance')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-surveillance.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Vidéosurveillance
+									</div>
+								</a>
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('#')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-menuiserie.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Menuiserie
+									</div>
+								</a>
+								<a class="nav-full-item" href="<?php echo esc_url(home_url('boutiques')); ?>">
+									<div class="nav-full-icon">
+										<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-boutique.svg"/>
+									</div>
+									<div class="nav-full-link">
+										Sécurité des boutiques
+									</div>
+								</a>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class="modal-footer">
-					<a class="btn btn-lg btn-primary" href="%3C?php%20echo%20esc_url(home_url('contact'));%20?%3E"><span class="material-icons">call</span><?php the_field('home_bouton_modal', 'option'); ?></a>
-					<p>pour en profiter</p>
 				</div>
 			</div>
 		</div>
