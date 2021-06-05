bc3f9e39867095e88c842096c2dbabc66a466639
digitalbrush <titus.nderitu@gmail.com>
2021-06-05 22:25:00 +0100

    Merge fix

--- global-templates/products-accessories.php
+++ global-templates/products-accessories.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 $container = get_theme_mod( 'understrap_container_type' );
 	?>
 
-<div class="section accessories">
+<div class="section section-accessories">
 	<div class="container-fluid" id="wrapper-static-content" tabindex="-1"  >
 		<div class="container">
 			<div id="accordionstore">
@@ -24,7 +24,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					</h5>
 					</div>
 
-					<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+					<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionstore">
 					<div class="card-body">
 						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 					</div>
@@ -39,7 +39,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						</button>
 					</h5>
 					</div>
-					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionstore">
 					<div class="card-body">
 						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 					</div>
