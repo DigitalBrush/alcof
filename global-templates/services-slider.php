3fd1b672ae28e26b9aa50b647caa31dedf9d2df8
digitalbrush <titus.nderitu@gmail.com>
2021-06-15 03:03:10 +0100

    Mmoved service slider to ACF

--- global-templates/services-slider.php
+++ global-templates/services-slider.php
@@ -20,17 +20,17 @@ $container = get_theme_mod( 'understrap_container_type' );
           <div class="col-sm-12">
             <div class="service-block">
               <div class="service-image">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/repair-man-dark-bg-web.jpg">
+                <img src="<?php the_field('diagnostic_image', 'option'); ?>">
               </div>
 
               <div class="service-cta">
-                <a class="btn btn-lg btn-primary" href="">En savoir plus</a>
+                <a class="btn btn-lg btn-primary" href="<?php the_field('diagnostic_link', 'option'); ?>">En savoir plus</a>
               </div>
 
               <div class="service-inner">
-                <h4 class="service-title">Diagnostic sécurité</h4>
-                <h6 class="service-subtitle">Un expert se déplace gratuitement</h6>
-                <p>Nos consultants se déplaçent gratuitement et à votre convenance. Leur rôle sera essentiellement de vous conseiller dans le choix de votre dispositif de sécurité. Forts d'une solide expérience des aspects techniques, financiers et esthétiques de la protection, ils étudieront votre demande, analyseront votre cahier des charges ainsi que votre budget et sauront vous orienter vers la solution la mieux adaptée à votre besoin. N'hésitez pas nous consulter. </p>
+                <h4 class="service-title"><?php the_field('diagnostic_title', 'option'); ?></h4>
+                <h6 class="service-subtitle"><?php the_field('diagnostic_subtitle', 'option'); ?></h6>
+                <p><?php the_field('diagnostic_description', 'option'); ?></p>
               </div>
             </div>
           </div>
@@ -38,17 +38,17 @@ $container = get_theme_mod( 'understrap_container_type' );
           <div class="col-sm-12">
             <div class="service-block">
               <div class="service-image">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/repair-man-dark-bg-web.jpg">
+                <img src="<?php the_field('installation_image', 'option'); ?>">
               </div>
 
               <div class="service-cta">
-                <a class="btn btn-lg btn-primary" href="installation">En savoir plus</a>
+                <a class="btn btn-lg btn-primary" href="<?php the_field('installation_link', 'option'); ?>">En savoir plus</a>
               </div>
 
               <div class="service-inner">
-                <h4 class="service-title">Installation</h4>
-                <h6 class="service-subtitle">Un travail d’expert sur mesure</h6>
-                <p>Pose de l’ensemble des produits de sécurité (serrure, porte-blindée, alarme, vidéo, sas etc.)</p>
+                <h4 class="service-title"><?php the_field('installation_title', 'option'); ?></h4>
+                <h6 class="service-subtitle"><?php the_field('installation_subtitle', 'option'); ?></h6>
+                <p><?php the_field('installation_description', 'option'); ?></p>
               </div>
             </div>
           </div>
@@ -56,22 +56,17 @@ $container = get_theme_mod( 'understrap_container_type' );
           <div class="col-sm-12">
             <div class="service-block">
               <div class="service-image">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/repair-man-dark-bg-web.jpg">
+                <img src="<?php the_field('serrurerie_image', 'option'); ?>">
               </div>
 
               <div class="service-cta">
-                <a class="btn btn-lg btn-primary" href="installation">En savoir plus</a>
+                <a class="btn btn-lg btn-primary" href="<?php the_field('serrurerie_link', 'option'); ?>">En savoir plus</a>
               </div>
 
               <div class="service-inner">
-                <h4 class="service-title">Serrurerie forte</h4>
-                <h6 class="service-subtitle">Le plus important point fort fichet d’Europe.</h6>
-                <p>La haute sécurité est assurée par les meilleurs fabriquants : Fichet, Guidotti, St-Gobain, Daitem. Notre équipe expérimentée de dessinateurs, techniciens et électromécaniciens réalise : </p>
-                <ul>
-                    <li>Conception et fabrication de sas</li>
-                    <li>Installation de bloc de porte de sécurité </li>
-                    <li>Pose miroiterie anti-effraction et pare-balle</li>
-                </ul>
+                <h4 class="service-title"><?php the_field('serrurerie_title', 'option'); ?></h4>
+                <h6 class="service-subtitle"><?php the_field('serrurerie_subtitle', 'option'); ?></h6>
+                <p><?php the_field('serrurerie_description', 'option'); ?></p>
               </div>
             </div>
           </div>
@@ -79,17 +74,35 @@ $container = get_theme_mod( 'understrap_container_type' );
           <div class="col-sm-12">
             <div class="service-block">
               <div class="service-image">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/repair-man-dark-bg-web.jpg">
+                <img src="<?php the_field('cles_image', 'option'); ?>">
               </div>
 
               <div class="service-cta">
-                <a class="btn btn-lg btn-primary" href="">En savoir plus</a>
+                <a class="btn btn-lg btn-primary" href="<?php the_field('cles_link', 'option'); ?>">En savoir plus</a>
               </div>
 
               <div class="service-inner">
-                <h4 class="service-title">Diagnostic sécurité</h4>
-                <h6 class="service-subtitle">Un expert se déplace gratuitement</h6>
-                <p>Nos consultants se déplaçent gratuitement et à votre convenance. Leur rôle sera essentiellement de vous conseiller dans le choix de votre dispositif de sécurité. Forts d'une solide expérience des aspects techniques, financiers et esthétiques de la protection, ils étudieront votre demande, analyseront votre cahier des charges ainsi que votre budget et sauront vous orienter vers la solution la mieux adaptée à votre besoin. N'hésitez pas nous consulter. </p>
+                <h4 class="service-title"><?php the_field('cles_title', 'option'); ?></h4>
+                <h6 class="service-subtitle"><?php the_field('cles_subtitle', 'option'); ?></h6>
+                <p><?php the_field('cles_description', 'option'); ?></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-12">
+            <div class="service-block">
+              <div class="service-image">
+                <img src="<?php the_field('financement_image', 'option'); ?>">
+              </div>
+
+              <div class="service-cta">
+                <a class="btn btn-lg btn-primary" href="<?php the_field('financement_link', 'option'); ?>">En savoir plus</a>
+              </div>
+
+              <div class="service-inner">
+                <h4 class="service-title"><?php the_field('financement_title', 'option'); ?></h4>
+                <h6 class="service-subtitle"><?php the_field('financement_subtitle', 'option'); ?></h6>
+                <p><?php the_field('financement_description', 'option'); ?></p>
               </div>
             </div>
           </div>
